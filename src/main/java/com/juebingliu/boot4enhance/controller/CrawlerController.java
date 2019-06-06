5074c98ba4a95ce33a56775fb4bff2cb511df9c3
juebingliu <farewe1l2018x@gmail.com>
2019-06-06 12:39:23 +0800

    swagger2集成

--- src/main/java/com/juebingliu/boot4enhance/controller/CrawlerController.java
+++ src/main/java/com/juebingliu/boot4enhance/controller/CrawlerController.java
@@ -3,6 +3,8 @@ package com.juebingliu.boot4enhance.controller;
 import com.alibaba.fastjson.JSON;
 import com.juebingliu.boot4enhance.domain.one.CrawlerContent;
 import com.juebingliu.boot4enhance.service.CrawlerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +22,14 @@ import java.util.Map;
  * @description
  */
 @RestController
+@Api(tags = "爬虫API")
 public class CrawlerController {
 
     @Autowired
     private CrawlerService crawlerService;
 
     @GetMapping("/getContent")
+    @ApiOperation(value="信息爬取")
     public String crawler() {
         String url = "http://www.gxhz.gov.cn";
 
@@ -43,6 +47,7 @@ public class CrawlerController {
     }
 
     @GetMapping("/compare")
+    @ApiOperation(value="信息比较")
     public String compare() {
         Map<String,String> map = crawlerService.compareContent();
         if(map != null && map.size() > 0) {
